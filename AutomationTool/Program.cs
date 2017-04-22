95976218f7548da08668a140d9019a39ed7b8e64
Ajith MK <aji4032@gmail.com>
2017-04-22 10:20:15 +0530

    First Commint

---
+++ AutomationTool/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //while()
+                //
+                
+                //if (key == "c")
+                //{
+                //    region test = new region(Utilities.CurrentMousePosition(), 100, 100);
+                //    Utilities.CaptureScreen(test, @"C:\Users\Aji\Desktop\test.png");
+                //    Console.WriteLine();
+                //}
+            }
+            //Console.WriteLine(Utilities.CurrentMousePosition());
+        }
+        private static void decideAction(string key)
+        {
+            switch (key.ToLower())
+            {
+                case "c":
+                    caseC();
+                    break;
+
+                case "d":
+                    caseD();
+                    break;
+
+                case "v":
+                    caseV();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid selection!");
+                    break;
+            }
+
+        }
+        private static void caseC()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutomationTool/Utilities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTool
+{
+    class point
+    {
+        public int x, y;
+        public point()
+        {
+            x = 0;
+            y = 0;
+        }
+        public point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class region
+    {
+        public point Location;
+        public int width, height;
+        public region(int width, int height)
+        {
+            Location = new point();
+            this.width = width;
+            this.height = height;
+        }
+        public region(point Location, int width, int height)
+        {
+            this.Location = Location;
+            this.width = width;
+            this.height = height;
+        }
+        public region(int x, int y, int width, int height)
+        {
+            Location = new point(x, y);
+            this.width = width;
+            this.height = height;
+        }
+    }
+    class Utilities
+    {
+        public static void CaptureScreen(region window, string file)
+        {
+            System.Threading.Thread.Sleep(3000);
+            try
+                {
+                Rectangle s_rect = new Rectangle(window.Location.x - (window.width/2), window.Location.y - (window.height/2), window.width, window.height);
+
+                using (Bitmap bmp = new Bitmap(s_rect.Width, s_rect.Height))
+                {
+                    using (Graphics gScreen = Graphics.FromImage(bmp))
+                        gScreen.CopyFromScreen(s_rect.Location, Point.Empty, s_rect.Size);
+                    bmp.Save(file, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+            catch (Exception) { /*TODO: Any exception handling.*/ }
+        }
+        public static point CurrentMousePosition()
+        {
+            point MouseLocation = new point();
+            MouseLocation.x = Cursor.Position.X;
+            MouseLocation.y = Cursor.Position.Y;
+            return MouseLocation;
+        }
+    }
+}
